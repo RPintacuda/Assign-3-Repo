97c4767f1eee6d8f1bb4cfb3e4ca3a1c887dbae3
Rachel Pintacuda <RPintacuda2020@fau.edu>
2023-04-23 20:23:17 -0400

    Added logic to display search input and display search result

--- run.py
+++ run.py
@@ -15,6 +15,20 @@ def index_documents(documents, index):
             print(f'Indexed {i} documents', end='\r')
     return index
 
+def execute_search(index,query,search_type,rank=False):
+    print(" ------------------------------------------------------------------------------------------------------- ")
+    print("query         = " + query)
+    print("search_type   = " + search_type)
+    print("Ranked Search = " + str(rank))
+    results = index.search(query, search_type,rank)
+    print("found "+str(len(results))+ " documents")
+    # print(results)
+    if rank:
+        for doc,score in results:
+            print('{0: <10} {1: <10} {2: <60} {3: <50}'.format(score, doc.ID, doc.title, doc.url).encode("utf-8"))
+    else:    
+        for doc in results:
+            print('{0: <10} {1: <60} {2: <50}'.format(doc.ID, doc.title, doc.url).encode("utf-8"))
 
 if __name__ == '__main__':
     # this will only download the xml dump if you don't have a copy already;
@@ -25,7 +39,7 @@ if __name__ == '__main__':
     index = index_documents(load_documents(), Index())
     print(f'Index contains {len(index.documents)} documents')
 
-    index.search('London Beer Flood', search_type='AND')
-    index.search('London Beer Flood', search_type='OR')
-    index.search('London Beer Flood', search_type='AND', rank=True)
-    index.search('London Beer Flood', search_type='OR', rank=True)
+    execute_search(index, 'London Beer Flood', search_type='AND')
+    execute_search(index, 'London Beer Flood', search_type='OR')
+    execute_search(index, 'London Beer Flood', search_type='AND', rank=True)
+    execute_search(index, 'London Beer Flood', search_type='OR', rank=True)
